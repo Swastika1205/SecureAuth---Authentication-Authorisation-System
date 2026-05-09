aaa9c1abd3935bcc90c7e1690c1e83d3488a46b5
Swastika Chatterjee <150961701+Swastika1205@users.noreply.github.com>
2026-05-09 16:32:43 +0530

    Initialize Express app with middleware and routes

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "SecureAuth API Running Successfully",
+  });
+});
+
+export default app;
